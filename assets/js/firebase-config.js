4f6d3b08ffddca06b6925a049d1f16f333e55a3a
Md. Afraim Bin Zahangir <42450202+Afraim@users.noreply.github.com>
2024-11-17 19:49:28 +0600

    firebase updated changed

--- assets/js/firebase-config.js
+++ assets/js/firebase-config.js
@@ -1,12 +1,12 @@
 var firebaseConfig = {
-    apiKey: "AIzaSyBDmKlsDYIK9qH4iaLurAyXwkbwaNRAHEg",
-    authDomain: "z-techx-contact.firebaseapp.com",
-    databaseURL: "https://z-techx-contact-default-rtdb.firebaseio.com/",
-    projectId: "z-techx-contact",
-    storageBucket: "z-techx-contact.firebasestorage.app",
-    messagingSenderId: "67204373050",
-    appId: "1:67204373050:web:d7cb293658e5e9918bb303",
-    measurementId: "G-QRCJ7XYCEC"
+    apiKey: "AIzaSyCqXkrZq6JzTQe8VvIzvYKIPehh7c5VKOw",
+    authDomain: "lms-ztechx.firebaseapp.com",
+    databaseURL: "https://lms-ztechx-default-rtdb.firebaseio.com",
+    projectId: "lms-ztechx",
+    storageBucket: "lms-ztechx.appspot.com",
+    messagingSenderId: "797261663120",
+    appId: "1:797261663120:web:46d1b826b9d2f345e20115",
+    measurementId: "G-P6VKP9J50X"
   };
   // Initialize Firebase
   try{
